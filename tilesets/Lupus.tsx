4b64743fbb1cdde24a3a77afc49968feb5349e14
Marco.Manzari <Marco.Manzari@quentic.com>
2023-12-07 15:54:42 +0100

    add testing lupus floor emblem

--- tilesets/Lupus.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.2" name="Lupus" tilewidth="32" tileheight="32" tilecount="336" columns="21">
- <image source="Lupus.png" trans="ffffff" width="696" height="538"/>
-</tileset>
